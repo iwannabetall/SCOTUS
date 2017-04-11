b90f845d70d620620a7bb7bcc6d13b16ddf010e0
Anna <iwannabetall@gmail.com>
2017-04-11 14:16:32 -0400

    slider

--- jquery.js
+++ jquery.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
+	//generate checkboxes for each year 
+	// for (var i = 1946; i < 2016; i++){
+	// 	$('.checkboxes').append('<input type ="checkbox" class="Yearbox" id=year' + i + " value=" + i + " onchange=" + '"processData()";><label class=' + 'yearlabel'+ '>' + i +'</label> <br>');	
+	// }
+	// $years = $.map($(Array(8)),function(val, i) { return i; });
+	// console.log($years)
 
-	for (var i = 1946; i < 2016; i++){
-		// var html = '<input type ="checkbox" id=year' + i + " value=" + i + " onchange=" + '"changeYear(' + i + ');><label class=' + 'yearlabel'+ '>' + i +'</label>';
-		// var html = html + html;
-		// console.log(html);
-		// // $('.checkboxes').html('<input type ="checkbox";><label class=' + 'yearlabel'+ '>' + "god dammit" +'</label>');
-// // <input type="checkbox" id="Martin O'Malley" value="Martin O'Malley" class="renderBox prelimInput" onchange="candidateProcessor();justChecked(this.value);"><label class="prelimLabel">O'Malley</label>
-		$('.checkboxes').append('<input type ="checkbox" class="Yearbox" id=year' + i + " value=" + i + " onchange=" + '"processData()";><label class=' + 'yearlabel'+ '>' + i +'</label>');	
-	}
+	 $(function () {
+
+        $("#range").ionRangeSlider({
+        	// values: $years,
+            hide_min_max: true,
+            keyboard: true,
+            min: 1946,
+            max: 2015,
+            from: 1975, //where slider starts 
+            to: 1980,
+            type: 'double',
+            step: 1,
+            grid: true,
+            prettify_enabled: false,
+            onStart: function (data){
+            	//process data with selected years
+            	processData(data.from, data.to);
+            },
+            onFinish: function (data) {
+            	processData();  
+            }
+        });
+
+    });
+
 });
-
-// function createNewCheckboxt(name, id){
-//     var checkbox = document.createElement('input'); 
-//     checkbox.type= 'checkbox';
-//     checkbox.value = id;
-//     checkbox.id = id;
-//     checkbox.label = name;	
-//     return checkbox;
-// }
-
-// for (var i = 1946; i < 2016; i++){
-// 	console.log(document.getElementById("checkboxes"));
-// 	// document.getElementById("checkboxes").appendChild(createNewCheckboxt(i, "year" + i));
-// }
-// });
